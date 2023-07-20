7f9d2634e4c44bd205e5329f7b5138adfafafa40
andrea <andrea.lacamera0@gmail.com>
2023-07-20 17:13:36 +0200

    modifiers

--- lesson4/classes.ts
+++ lesson4/classes.ts
@@ -1,17 +1,20 @@
 class User {
-  readonly hiddenID: string = "XYZ"
-  name: string
-  email: string
+  private readonly hiddenID: string 
+  public name: string
+  public email: string
 
   constructor(n: string, e: string) {
     this.name = n
     this.email = e
+    this.hiddenID = "XYZ"
   }
 
-  err() {
-    // this.hiddenID = "QWERTY" // error! the field cannot modified outside the constructor!
-  }
+  // err() {
+  //   this.hiddenID = "QWERTY" // error! the field cannot modified outside the constructor!
+  // }
 }
 
 const u = new User("Andrea", "andrea@teiga.it")
-// u.hiddenID = "QWERTY" // error! the field cannot modified outside the class!
+// u.hiddenID = "QWERTY" // error! the field is private and is only accessible within the class!
+u.name = "Tom"
+u.email = "tom@tom.com"
