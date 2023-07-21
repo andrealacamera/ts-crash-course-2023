4fc964d08f9200a608e49153323f1fd23dd04711
andrea <andrea.lacamera0@gmail.com>
2023-07-21 22:23:40 +0200

    generics - end

--- lesson4/generics.ts
+++ lesson4/generics.ts
@@ -43,5 +43,51 @@ const getMoreSomething2 = <T>(products: T[]): T => products[0]
 // When we use JSX / TSX syntax (React, etc) it's common to find a comma after the <Type> (i.e. <Type,>).It's done to understand it's a type parameter and not a JSX element
 
 
+// Remark! We can use different name for the generic type parameter, until the number of type variables and how the type variables line up.
+//Example
+function identity2<Input>(arg: Input): Input {
+  return arg
+}
+
+// We can also use more Types, separated by a comma (as in the JSX remark here above)
+function fff<T,U>(arg: T, arg2: U): object {
+  return {
+    one: arg,
+    two: arg2
+  }
+}
 
 
+// Generic Classes:  First we define two interfaces, then we create a generic class to instance different objects.
+interface Course {
+  name: string,
+  author: string,
+  subject: string
+}
+
+interface Quiz {
+  name: string,
+  type: string
+}
+
+class Sellable<T> {
+  public cart: T[] = []
+  // any products can be placed on the cart
+  addToCart(product: T) {
+    this.cart.push(product)
+    console.log(this.cart)
+  }
+}
+
+let x = new Sellable()
+let qq: Quiz = {
+  name: "quiz1",
+  type: "quiz"
+}
+let cc: Course = {
+  name: "course JS",
+  author: "Andrea",
+  subject: "-"
+}
+x.addToCart(qq)
+x.addToCart(cc)
